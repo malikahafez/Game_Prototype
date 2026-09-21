606d6f6838454b8c6e969b95f0eb090687542558
Malika Hafez <malikamhafez@gmail.com>
2026-09-22 01:36:45 +0300

    Create raycasting script, generate colliders, code interaction

---
+++ Assets/Scripts/Raycasting.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Raycasting : MonoBehaviour
+{public static float distanceFromTarget;
+    [SerializeField] float toTarget;
+    void Update()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit))
+        {
+            toTarget = hit.distance;
+            distanceFromTarget = hit.distance;
+        }
+    }
+}
--- Assets/Scripts/crateOpen.cs
+++ Assets/Scripts/crateOpen.cs
@@ -4,23 +4,43 @@ public class crateOpen : MonoBehaviour
 {
     [SerializeField] bool latch = false;
     [SerializeField] bool crate = false;
+    [SerializeField] float internalDistance;
     
     // Update is called once per frame
     void Update()
     {
-        if(latch){
-            OpenLatch();
+        // if(latch){
+        //     OpenLatch();
+        // }
+        // if(crate){
+        //     OpenCrate();
+        // }
+        internalDistance = Raycasting.distanceFromTarget;
+        if(latch == false && crate ==false && internalDistance < 3)
+        {
+            if (Input.GetKeyDown(KeyCode.O))
+            {
+                latch = true;
+                OpenLatch();
+            }
+            
         }
-        if(crate){
-            OpenCrate();
+        if(latch == true && crate == false && internalDistance < 3)
+        {
+            if (Input.GetKeyDown(KeyCode.O))
+            {
+                crate = true;
+                OpenCrate();
+            }
+            
         }
     }
 
     public void OpenLatch(){
-        this.GetComponent<Animator>().Play("crate_open", -1, 0f);
+        this.GetComponent<Animator>().Play("crate_open", 0, 0f);
     }
 
      public void OpenCrate(){
-        this.GetComponent<Animator>().Play("crate_open2", 0, 0f);
+        this.GetComponent<Animator>().Play("crate_open2", 1, 0f);
     }
 }
